c734c25306c65674556ccefa4c536e934d9d2a13
Igor_nasc <igor_flipe@hotmail.com>
2020-07-07 16:22:47 -0300

    Refatoração classe Autor e criação da classe Categoria e coleção de categorias

---
+++ test/test_category.py
@@ -0,0 +1,20 @@
+import unittest
+from datetime import datetime as dt
+
+from src.category import Category
+
+
+class MyTestCategory(unittest.TestCase):
+
+    def test_CreateCategory_NoneName_RaiseException(self):
+        name = None
+        self.assertRaises(ValueError, Category, name)
+
+    def test_CreateCategory_TimeSave(self):
+        name = 'Data Science'
+        category = Category(name)
+        self.assertEqual(str(dt.today()), category.time)
+
+
+if __name__ == '__main__':
+    unittest.main()
