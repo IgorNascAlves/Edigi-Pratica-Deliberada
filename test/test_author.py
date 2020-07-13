a850c35323972098d888b780e00c1322655ab960
Igor_nasc <igor_flipe@hotmail.com>
2020-07-13 15:54:00 -0300

    reorganização dos testes

--- test/test_author.py
+++ test/test_author.py
@@ -6,6 +6,12 @@ from src.author import Author
 
 class TestCaseAuthor(unittest.TestCase):
 
+    def test_CreateAuthor_TimeSave(self):
+        name = 'Igor do Nascimento Alves'
+        email = 'igor.nascimento.flipe@gmail.com'
+        author1 = Author(name, email)
+        self.assertEqual(str(dt.today()), author1.time)
+
     def test_CreateAuthor_NoneName_RaiseException(self):
         name = None
         email = 'igor.nascimento.flipe@gmail.com'
@@ -15,12 +21,6 @@ class TestCaseAuthor(unittest.TestCase):
         name = 'Igor do Nascimento Alves'
         email = 'igor.nascimento.flipe@.com'
         self.assertRaises(ValueError, Author, name, email)
-
-    def test_CreateAuthor_TimeSave(self):
-        name = 'Igor do Nascimento Alves'
-        email = 'igor.nascimento.flipe@gmail.com'
-        author1 = Author(name, email)
-        self.assertEqual(str(dt.today()), author1.time)
 
 
 if __name__ == '__main__':
