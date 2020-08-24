218242f7ec950f8b080b9ebf3b3645debf9e7a66
Igor_nasc <igor_flipe@hotmail.com>
2020-08-24 13:57:45 -0300

    refatoração do calculo do total

--- src/cart.py
+++ src/cart.py
@@ -7,7 +7,6 @@ from typing import Dict, List
 class Cart:
     def __init__(self):
         self.__items: List = []
-        self.__total: int = 0
         self.__time: str = ""
 
     def add(self, book: Book, amount: int) -> None:
@@ -17,16 +16,17 @@ class Cart:
 
         self.__items.append((book, amount))
 
-    def calculate_the_total(self):
+    def calculate_the_total(self) -> int:
+        total: int = 0
         for book, amount in self.__items:
-            self.__total += book.price * amount
+            total += book.price * amount
+        return total
 
     def close(self) -> Dict:
 
         resume = {item[0].title: item[1] for item in self.__items}
 
-        self.calculate_the_total()
-        resume['total'] = self.__total
+        resume['total'] = self.calculate_the_total()
 
         self.__time = str(dt.today())
 
